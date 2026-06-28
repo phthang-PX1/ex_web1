ac6547b779fc2f3f8a70e7e417b09a65095c2ba9
phthang-PX1 <phthang0872@gmail.com>
2026-06-28 08:26:14 +0700

    Bai 61,62

---
+++ Ex62/scripts.js
@@ -0,0 +1,9 @@
+// Hàm xử lý sự kiện Mouse Over (Di chuyển chuột vào dòng thì tô nền vàng)
+function toNenVang(row) {
+    row.style.backgroundColor = "yellow";
+}
+
+// Hàm xử lý sự kiện Mouse Leave (Di chuyển chuột ra thì tô nền xanh)
+function toNenXanh(row) {
+    row.style.backgroundColor = "blue";
+}
